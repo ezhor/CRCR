651c3303b7585175415a895efd4c66d2d8945fe8
Ézhor Malkávar <ezhormalkavar@gmail.com>
2019-03-01 00:32:23 +0100

    Baud rate and serial ports corrected for Leonardo

--- CRCR-arduino/CRCR-arduino.ino
+++ CRCR-arduino/CRCR-arduino.ino
@@ -1,26 +1,19 @@
 // La velocidad depende del modelo de ESP-01
 // siendo habituales 9600 y 115200
-const int baudRate = 115200;
+#define BAUD_RATE_SERIAL 9600
+#define BAUD_RATE_SERIAL1 115200
  
-#include "SoftwareSerial.h"
-SoftwareSerial softSerial(2, 3); // RX, TX
- 
-void setup()
-{
-   Serial.begin(baudRate);
-   softSerial.begin(baudRate);
+
+void setup()  {
+  Serial.begin(BAUD_RATE_SERIAL);
+  Serial1.begin(BAUD_RATE_SERIAL1);
 }
+void loop()  {
+   if (Serial1.available()) {
+      Serial.write((char)Serial1.read());
+   }
  
-void loop()
-// enviar los datos de la consola serial al ESP-01, 
-// y mostrar lo enviado por el ESP-01 a nuestra consola
-{
-   if (softSerial.available())
-   {
-      Serial.print((char)softSerial.read());
-   }
-   if (Serial.available())
-   {
-      softSerial.print((char)Serial.read());
-   }
+  if (Serial.available()) {
+     Serial1.write((char)Serial.read());
+  }
 }
